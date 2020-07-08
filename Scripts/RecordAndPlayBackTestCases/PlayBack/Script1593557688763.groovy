3e3253674d7eb87e3536dbd7d6d3dbda313c9748
sam <sabtester@yahoo.com>
2020-07-08 17:49:28 +0100

    initial commit

---
+++ Scripts/RecordAndPlayBackTestCases/PlayBack/Script1593557688763.groovy
@@ -0,0 +1,46 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.facebook.com/')
+
+WebUI.setText(findTestObject('FacebookWebElements/Page_Facebook  log in or sign up/input_Password_email'), 'sabtester')
+
+WebUI.setText(findTestObject('FacebookWebElements/Page_Facebook  log in or sign up/input_concat(We couldn  t create your accou_184fe7'), 
+    'sam')
+
+WebUI.setText(findTestObject('FacebookWebElements/Page_Facebook  log in or sign up/input_concat(We couldn  t create your accou_e7d8cc'), 
+    'badu')
+
+WebUI.setText(findTestObject('FacebookWebElements/Page_Facebook  log in or sign up/input_concat(We couldn  t create your accou_33dd4d'), 
+    '09877776666')
+
+WebUI.selectOptionByValue(findTestObject('FacebookWebElements/Page_Facebook  log in or sign up/select_Day123456789101112131415161718192021_40ab5b'), 
+    '26', true)
+
+WebUI.selectOptionByValue(findTestObject('FacebookWebElements/Page_Facebook  log in or sign up/select_MonthJanFebMarAprMayJunJulAugSeptOctNovDec'), 
+    '11', true)
+
+WebUI.selectOptionByValue(findTestObject('FacebookWebElements/Page_Facebook  log in or sign up/select_Year20202019201820172016201520142013_c76834'), 
+    '1980', true)
+
+WebUI.click(findTestObject('FacebookWebElements/Page_Facebook  log in or sign up/input_Female_sex'))
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/SeleniumTest/allTest.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>allTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>50e36ac3-13b0-447e-b772-0142a3f7cf90</testSuiteGuid>
+   <testCaseLink>
+      <guid>fc5d0625-21ec-4b7d-bd55-ff449d61d400</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ManualMode/ManualModeTestCase</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>da7b9705-fbd2-47c2-bfc3-f8e645e15197</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RecordAndPlayBackTestCases/PlayBack</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9db0a9bd-734a-475d-8f90-868847905339</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ScriptMode/ScriptModeTestCase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
